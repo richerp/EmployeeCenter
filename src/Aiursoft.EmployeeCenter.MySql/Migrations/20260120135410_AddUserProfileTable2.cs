c9418d54b7505a09dae9483c7499728777112ac6
Anduin <anduin@aiursoft.com>
2026-01-20 13:56:08 +0000

    Fix migrations.

---
+++ src/Aiursoft.EmployeeCenter.MySql/Migrations/20260120135410_AddUserProfileTable2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.EmployeeCenter.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfileTable2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LegalRepresentativeLegalName",
+                table: "CompanyEntities",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LegalRepresentativeLegalName",
+                table: "CompanyEntities");
+        }
+    }
+}
---
+++ src/Aiursoft.EmployeeCenter.Sqlite/Migrations/20260120135335_AddUserProfileTable2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.EmployeeCenter.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfileTable2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LegalRepresentativeLegalName",
+                table: "CompanyEntities",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LegalRepresentativeLegalName",
+                table: "CompanyEntities");
+        }
+    }
+}
